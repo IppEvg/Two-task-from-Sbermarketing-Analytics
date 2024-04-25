d2cba529d491b191b7316fdfd917709fb7ee2aee
Evgeniy Ippolitov <ippevg@yandex.ru>
2024-04-25 16:02:07 +0300

    Start of the test work

--- task_1/index.js
+++ task_1/index.js
@@ -1,8 +1,23 @@
 import {encoded, translations} from './data.js'
+const translationsArr = Object.entries(translations)
+const uniqId = []
+let decoded = encoded.map(objectItem=>{
+    for(let el in objectItem){
+        const decodedItem = translationsArr.find(item=>item[0]==objectItem[el])
+        // проверяю все id не входящие в translations. Без уточнения не понял, нужно ли было 
+        // проверять уникальность на повторения во всех объектах друг с другом или только 
+        // сравнивая с translations..
+        if((/\w+Id/).test(el) && !decodedItem && !uniqId.includes(objectItem[el])){
+            uniqId.push(objectItem[el])
+        }
+        if(el==='groupId' || el==='service' || el==='formatSize' || el==='ca'){
+        } else {
+            objectItem[el]=decodedItem?decodedItem[1]:objectItem[el]
+        }
+    }
+    return objectItem
+})
 
 console.log("Let's rock")
 console.log(encoded, translations)
-
-
-
-// console.log(decoded)
+console.log(decoded,'\n',`Список id, не входящие в translations: ${uniqId}`)
--- task_2/src/lib/Counter.svelte
+++ task_2/src/lib/Counter.svelte
@@ -1,10 +1,13 @@
 <script lang="ts">
+import {onMount} from {svelte}
   let count: number = 0
   const increment = () => {
     count += 1
   }
+  const allCurrences = []
+  onMount(()=>{
+  let promise = async(
+  )
 </script>
 
-<button on:click={increment}>
-  count is {count}
-</button>
+
